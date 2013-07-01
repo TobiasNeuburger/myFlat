74069c679362622785fa5d0312567b7eb8403009
TobiasNeuburger <tobi.neuburger@gmail.com>
2013-07-01 16:39:31 +0200

    Dashboard and borders...

--- myFlat/gen/com/example/myflat/R.java
+++ myFlat/gen/com/example/myflat/R.java
@@ -21,29 +21,36 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int drop=0x7f020000;
-        public static final int energy=0x7f020001;
-        public static final int fire=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int logo=0x7f020004;
+        public static final int border=0x7f020000;
+        public static final int drop=0x7f020001;
+        public static final int energy=0x7f020002;
+        public static final int fire=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int logo=0x7f020005;
+        public static final int logout=0x7f020006;
+        public static final int settings=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000f;
-        public static final int login_action=0x7f080003;
-        public static final int login_logo=0x7f080000;
-        public static final int login_mail=0x7f080001;
-        public static final int login_pass=0x7f080002;
-        public static final int login_question=0x7f080005;
-        public static final int login_register=0x7f080004;
-        public static final int login_solution=0x7f080006;
-        public static final int register_action=0x7f08000b;
-        public static final int register_firstname=0x7f080007;
-        public static final int register_lastname=0x7f080008;
-        public static final int register_login=0x7f08000c;
-        public static final int register_login_question=0x7f08000d;
-        public static final int register_login_solution=0x7f08000e;
-        public static final int register_mail=0x7f080009;
-        public static final int register_pass=0x7f08000a;
+        public static final int action_settings=0x7f080012;
+        public static final int edittext=0x7f080002;
+        public static final int imageView1=0x7f080000;
+        public static final int login_action=0x7f080006;
+        public static final int login_logo=0x7f080003;
+        public static final int login_mail=0x7f080004;
+        public static final int login_pass=0x7f080005;
+        public static final int login_question=0x7f080008;
+        public static final int login_register=0x7f080007;
+        public static final int login_solution=0x7f080009;
+        public static final int logout=0x7f080013;
+        public static final int register_action=0x7f08000e;
+        public static final int register_firstname=0x7f08000a;
+        public static final int register_lastname=0x7f08000b;
+        public static final int register_login=0x7f08000f;
+        public static final int register_login_question=0x7f080010;
+        public static final int register_login_solution=0x7f080011;
+        public static final int register_mail=0x7f08000c;
+        public static final int register_pass=0x7f08000d;
+        public static final int textView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_dashboard=0x7f030000;
@@ -56,25 +63,36 @@ public final class R {
         public static final int register=0x7f070002;
     }
     public static final class string {
+        /**  Strings for Actionbar 
+         */
         public static final int action_settings=0x7f050001;
+        public static final int actions_logout=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f05000e;
-        public static final int login_login=0x7f050003;
+        public static final int dashboard_energy=0x7f050011;
+        /**  Strings for Dashboard 
+         */
+        public static final int dashboard_gas=0x7f050010;
+        public static final int dashboard_logout=0x7f050013;
+        public static final int dashboard_settings=0x7f050014;
+        public static final int dashboard_water=0x7f050012;
+        public static final int hello_world=0x7f05000f;
+        public static final int login_login=0x7f050004;
         /**  Strings for Login Form 
          */
-        public static final int login_logo=0x7f050002;
-        public static final int login_mail=0x7f050006;
-        public static final int login_pass=0x7f050007;
-        public static final int login_register_question=0x7f050004;
-        public static final int login_register_solution=0x7f050005;
-        public static final int register_back_question=0x7f050009;
-        public static final int register_back_solution=0x7f05000a;
-        public static final int register_firstname=0x7f05000b;
-        public static final int register_lastname=0x7f05000c;
+        public static final int login_logo=0x7f050003;
+        public static final int login_mail=0x7f050007;
+        public static final int login_pass=0x7f050008;
+        public static final int login_register_question=0x7f050005;
+        public static final int login_register_solution=0x7f050006;
+        public static final int register_back_question=0x7f05000a;
+        public static final int register_back_solution=0x7f05000b;
+        public static final int register_firstname=0x7f05000c;
+        public static final int register_lastname=0x7f05000d;
         /**  Strings for Register Form 
          */
-        public static final int register_register=0x7f050008;
-        public static final int title_activity_dashboard=0x7f05000d;
+        public static final int register_register=0x7f050009;
+        public static final int title_activity_dashboard=0x7f05000e;
+        public static final int title_activity_test=0x7f050015;
     }
     public static final class style {
         /** 
--- myFlat/src/com/example/myflat/Login.java
+++ myFlat/src/com/example/myflat/Login.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class Login extends Activity {
@@ -23,6 +24,17 @@ public class Login extends Activity {
         		startActivity(i);
 			}
 			
+		});
+		
+		Button loginButton = (Button) findViewById(R.id.login_action);
+		loginButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(getApplicationContext(), Dashboard.class);
+        		startActivity(i);
+				
+			}
 		});
 	}
 
