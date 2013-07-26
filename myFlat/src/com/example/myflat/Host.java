0e122536c78af0f74f75694eda0ad159350bff6a
Florian Imgrund <florian.imgrund@gmail.com>
2013-07-26 21:08:33 +0200

    test2

--- myFlat/src/com/example/myflat/Host.java
+++ myFlat/src/com/example/myflat/Host.java
@@ -2,7 +2,7 @@ package com.example.myflat;
 
 public class Host {
 	
-	private static final String HOST = "10.31.2.3";
+	private static final String HOST = "192.168.2.103";
 	
 	public String getHost() {
 		return HOST;
